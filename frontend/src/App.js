d91d25be46b63d4b2d0fbe965819897e8fc16d4b
MadamHippo <88348716+MadamHippo@users.noreply.github.com>
2022-02-28 18:41:04 -0800

    React bootstrap and bootswatch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
+// App.js is the parent component, we attached footer and header subcomps in this below.
+
 // arrow functions - new way to write functions in ES6
+import {Container} from 'react-bootstrap';
+import Footer from "./components/Footer";
+import { Header } from "./components/Header";
 
  const App = () => {
    return (
-   <>
-      <h1>Welcome to MyShop!</h1>
+    <>
+      <Header />
+      <main>
+        <Container>
+        <h1>Welcome to MyShop!</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
  }
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>Header</header>
+  )
+}
+
+// racfe 
+// put empty fragment you can use as many divs as possible but the other one must have just 1 main div.
