67b893e00eef33333dea596c47b049d14408bd2d
MadamHippo <88348716+MadamHippo@users.noreply.github.com>
2022-03-07 01:03:58 -0800

    Setting up React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,32 @@
 // arrow functions - new way to write functions in ES6
 import {Container} from 'react-bootstrap';
 import Footer from "./components/Footer";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom' // See app below how router, and routes, and route is bundled and organized together
 import { Header } from "./components/Header";
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
+
+
+//In situation where someone has multiple slashes we need to put 'exact' so if you see another path with another slash that says product we need to take them to that
 
 //arrow functions
 
  const App = () => {
    return (
     <>
+    <Router>
       <Header />
-      <main>
+      <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
+    </Router>
     </>
   );
  }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,24 +1,33 @@
 import React from 'react'
 import {Navbar, Container, Nav} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 
 export const Header = () => {
   return (
     <header>
     <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
       <Container>
-        <Navbar.Brand href="/"> REACT ART GALLERY & SHOP </Navbar.Brand>
+        <LinkContainer to="/">
+        <Navbar.Brand> REACT ART GALLERY & SHOP </Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
-            <Nav.Link href="/login"><i className='fas fa-user'></i> Sign In</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  </header>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link ><i className='fas fa-user'></i> Sign In</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   )
 }
+
+export default Header
 
 // racfe 
 // put empty fragment you can use as many divs as possible but the other one must have just 1 main div.
