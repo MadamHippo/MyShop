46bb4f2d1f2b7d305c0fe3c4c820a80480e588c2
MadamHippo <88348716+MadamHippo@users.noreply.github.com>
2022-04-02 13:16:44 -0700

    Loader.js, messages.js, homescreen and product screen changes

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import {Row, Col} from 'react-bootstrap'
 import Product from '../components/Product'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 import { listProducts } from '../actions/productActions'
-import { useDispatch, useSelector } from 'react-redux'
 
 
 // Hook is sort of like the USB port to a laptop. Component is like the laptop. These Hooks are available for you to plugin. It's like an extra route that's enhancing what's originally there in the component state. Hooks will route to the USB device, read the data, and give you the data back/make the data available for you. Hooks will go "OK you need me to do this and I'll listen for these things you're telling me to watch out for and give it back to you."
@@ -25,22 +27,22 @@ const HomeScreen = () => {
   return (
     // using the Map function omg finally using algo stuff!! make a loop
     <>
-      <h1>Latest Products</h1>
-      {loading ? (
-        <h2>Loading...</h2>
-      ) : error ? (
-        <h3>{error}</h3>
-      ): (<Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>)}
+    <h1>Latest Products</h1>
+    {loading ? (
+      <Loader />
+    ) : error ? (
+      <Message variant='danger'>{error}</Message>
+    ): (<Row>
+      {products.map((product) => (
+        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+          <Product product={product} />
+        </Col>
+      ))}
+    </Row>)}
 
-      
-    </>
-  )
+    
+  </>
+)
 }
 
 export default HomeScreen
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,8 +1,11 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import React, {useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating';
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { listProductDetails } from '../actions/productActions'
 
 // Use State hook, store state information within the component so the compoenent can use it. 
 // Now pass the State information to the other components (as many components as I want). Globally sharing the State with the other components of this app = REDUX!! It's called Redux. State is temp. storage of data. Now Redux is a GLOBAL memory, a temporary store, it's not a database.
@@ -11,32 +14,24 @@ import Rating from '../components/Rating';
 
 const ProductScreen = () => {
   const params = useParams();
-  const [product, setProduct] = useState({})
+  const dispatch = useDispatch()
+  const productDetails = useSelector((state) => state.productDetails)
+  const { loading, error, product } = productDetails
 
   useEffect(() => {
-    const fetchProduct = async () => {
-      const {data} = await axios.get(`/api/products/${params.id}`)
+    dispatch(listProductDetails(params.id))
+  }, [dispatch, params])
 
-      setProduct(data)
-    }
-
-    fetchProduct()
-  }, [params])
-
-// Params = parameters stored in product.js - can be like id or location. Parameteres are all a collection.    
-  // params is coming from actual params by making a call to the API and it will come back as response Json and then call the function setProduct as part of useState. Now you're done hooking things up
-//  find method, within the aray it will loop through and find the element we are looking for. We are looking for product, and in this case product.id. FInd will loop through each element in products.js which is coming thorugh the params above. This guy will go find the product and find the id. Then we display it below the HTML required for it to display on the React page.
-
-  // this is a function page, we have to return something here
-
-  // params in fetchProduct will refresh and fetch new id (products) which is what params stands for. It will rerun useEffect to fetch whatever new param value change it is you want.
-
-  
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>
         Go Back
       </Link>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
       <Row>
         <Col md={6}>
           <Image src={product.image} alt={product.name} fluid />
@@ -88,9 +83,17 @@ const ProductScreen = () => {
             </ListGroup>
           </Card>
         </Col>
-      </Row>
+      </Row>)}
     </>
   )
 }
 
 export default ProductScreen
+
+// Params = parameters stored in product.js - can be like id or location. Parameteres are all a collection.    
+  // params is coming from actual params by making a call to the API and it will come back as response Json and then call the function setProduct as part of useState. Now you're done hooking things up
+//  find method, within the aray it will loop through and find the element we are looking for. We are looking for product, and in this case product.id. FInd will loop through each element in products.js which is coming thorugh the params above. This guy will go find the product and find the id. Then we display it below the HTML required for it to display on the React page.
+
+  // this is a function page, we have to return something here
+
+  // params in fetchProduct will refresh and fetch new id (products) which is what params stands for. It will rerun useEffect to fetch whatever new param value change it is you want.
